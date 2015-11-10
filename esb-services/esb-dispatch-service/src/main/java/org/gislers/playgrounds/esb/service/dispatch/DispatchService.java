40afb1bda9be4ec1e0794b808a22b6f3c35bbbea
James Gisler <james.gisler@nike.com>
2015-11-10 14:47:11 -0800

    closing jax-rs client

--- esb-services/esb-dispatch-service/src/main/java/org/gislers/playgrounds/esb/service/dispatch/DispatchService.java
+++ esb-services/esb-dispatch-service/src/main/java/org/gislers/playgrounds/esb/service/dispatch/DispatchService.java
@@ -2,14 +2,10 @@ package org.gislers.playgrounds.esb.service.dispatch;
 
 import org.gislers.playgrounds.esb.service.dispatch.dto.DispatchServiceDto;
 
-import javax.ejb.Local;
-
 /**
  * Created by:   jgisle
  * Created date: 10/15/15
  */
-@Local
 public interface DispatchService {
-
     void dispatchMessage( DispatchServiceDto dispatchServiceDto );
 }
--- esb-services/esb-dispatch-service/src/main/java/org/gislers/playgrounds/esb/service/dispatch/DispatchServiceBean.java
+++ esb-services/esb-dispatch-service/src/main/java/org/gislers/playgrounds/esb/service/dispatch/DispatchServiceBean.java
@@ -1,13 +1,13 @@
-package org.gislers.playgrounds.esb.service.dispatch.bean;
+package org.gislers.playgrounds.esb.service.dispatch;
 
 import org.gislers.playgrounds.esb.common.message.MessageConstants;
-import org.gislers.playgrounds.esb.service.dispatch.DispatchService;
 import org.gislers.playgrounds.esb.service.dispatch.dto.DispatchServiceDto;
 import org.gislers.playgrounds.esb.service.dispatch.exception.DispatchServiceException;
 import org.gislers.playgrounds.esb.service.dispatch.service.EndpointLookupService;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
@@ -23,7 +23,7 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 @Stateless
 public class DispatchServiceBean implements DispatchService {
 
-    private static final Logger logger = Logger.getLogger(DispatchServiceBean.class.getSimpleName());
+    private static final Logger logger = Logger.getLogger(DispatchServiceBean.class.getName());
 
     @Inject
     private EndpointLookupService endpointLookupService;
@@ -37,9 +37,15 @@ public class DispatchServiceBean implements DispatchService {
             throw new DispatchServiceException("Endpoint not configured: " + dispatchServiceDto.toString());
         }
         else {
+            Client client = null;
             Response response = null;
             try {
-                response = sendMessage(endpoint, dispatchServiceDto);
+                client = ClientBuilder.newClient();
+                response = client.target(endpoint)
+                        .request()
+                        .header(MessageConstants.TRANSACTION_ID, dispatchServiceDto.getTxId())
+                        .post(Entity.entity(dispatchServiceDto.getPayload(), MediaType.APPLICATION_JSON_TYPE), Response.class);
+
                 if (response != null && !isSuccess(response)) {
                     logger.info(dispatchServiceDto.getPayload() + " - " + response.toString());
                 }
@@ -48,16 +54,12 @@ public class DispatchServiceBean implements DispatchService {
                 if( response != null ) {
                     response.close();
                 }
+
+                if( client != null ) {
+                    client.close();
+                }
             }
         }
-    }
-
-    Response sendMessage(String endpoint, DispatchServiceDto dispatchServiceDto) {
-        return ClientBuilder.newClient()
-                .target(endpoint)
-                .request()
-                .header(MessageConstants.TRANSACTION_ID, dispatchServiceDto.getTxId())
-                .post(Entity.entity(dispatchServiceDto.getPayload(), MediaType.APPLICATION_JSON_TYPE), Response.class);
     }
 
     boolean isSuccess( Response response ) {
