c9cb006eee2ba6521112f58d6b4c858309e59069
James Gisler <james.gisler@nike.com>
2015-11-10 13:01:53 -0800

    Added @Produces method for Logger

--- esb-gateway/esb-gateway-web/src/main/java/org/gislers/playgrounds/esb/gateway/GatewayApplication.java
+++ esb-gateway/esb-gateway-web/src/main/java/org/gislers/playgrounds/esb/gateway/GatewayApplication.java
@@ -1,7 +1,10 @@
 package org.gislers.playgrounds.esb.gateway;
 
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.logging.Logger;
 
 /**
  * Created by:   jgisle
@@ -9,5 +12,10 @@ import javax.ws.rs.core.Application;
  */
 @ApplicationPath("/api")
 public class GatewayApplication extends Application {
+
+    @Produces
+    public Logger logger(InjectionPoint injectionPoint) {
+        return Logger.getLogger( injectionPoint.getMember().getDeclaringClass().getName() );
+    }
 
 }
